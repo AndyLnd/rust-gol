d3212a33d0e3433be9699ed33285c1e13573bfba
AndyLnd <andy@wire.com>
2018-10-16 21:56:45 +0200

    Added CellMap type

--- src/main.rs
+++ src/main.rs
@@ -14,71 +14,66 @@ use gtk::DrawingArea;
 
 use cairo::Context;
 
+type CellMap = Vec<Vec<bool>>;
+
 struct Cells {
-    current: Vec<bool>,
-    next: Vec<bool>,
+    current: CellMap,
+    next: CellMap,
     width: u32,
     height: u32,
-    size: u32,
 }
 
 impl Cells {
     fn new(width: u32, height: u32) -> Self {
-        let size = width * height;
         Cells {
-            current: Self::generate_random(size),
-            next: vec![false; size as usize],
+            current: Self::generate_random(width, height),
+            next: vec![vec![false; height as usize]; width as usize],
             width: width,
             height: height,
-            size: size,
         }
     }
     fn swap(&mut self) {
         ::std::mem::swap(&mut self.current, &mut self.next);
     }
-    fn borrow_cells<'a>(&'a self) -> &'a Vec<bool> {
+    fn borrow_cells<'a>(&'a self) -> &'a CellMap {
         &self.current
     }
     fn update(&mut self) {
         self.step();
         self.swap();
     }
-    fn randomize(&mut self) {
-        self.next = (0..self.size)
-            .map(|_| rand::random::<bool>())
-            .collect::<Vec<bool>>();
-    }
     fn step(&mut self) {
-        self.next = (0..self.size)
-            .map(|index| {
-                let n = |x: u32, y: u32| self.current[(x + y * self.width) as usize] as i32;
-                let x0 = (index + self.width - 1) % self.width;
-                let x1 = index % self.width;
-                let x2 = (index + 1) % self.width;
-                let y0 = ((index + self.size - self.width) / self.width) % self.height;
-                let y1 = index / self.width;
-                let y2 = ((index + self.width) / self.width) % self.height;
-                let neighbor_count = n(x0, y0)
-                    + n(x1, y0)
-                    + n(x2, y0)
-                    + n(x0, y1)
-                    + n(x2, y1)
-                    + n(x0, y2)
-                    + n(x1, y2)
-                    + n(x2, y2);
-                let is_alive = self.current[(x1 + y1 * self.width) as usize];
-                match neighbor_count {
-                    n if n < 2 => false,
-                    2 if is_alive => true,
-                    3 => true,
-                    _ => false,
-                }
+        self.next = (0..self.width)
+            .map(|x1| {
+                (0..self.height)
+                    .map(|y1| {
+                        let x0 = (x1 + self.width - 1) % self.width;
+                        let x2 = (x1 + 1) % self.width;
+                        let y0 = (y1 + 1) % self.height;
+                        let y2 = (y1 + self.height - 1) % self.height;
+
+                        let n = |x: u32, y: u32| self.current[x as usize][y as usize] as i32;
+                        let n_count = n(x0, y0)
+                            + n(x1, y0)
+                            + n(x2, y0)
+                            + n(x0, y1)
+                            + n(x2, y1)
+                            + n(x0, y2)
+                            + n(x1, y2)
+                            + n(x2, y2);
+                        let is_alive = self.current[x1 as usize][y1 as usize];
+                        match n_count {
+                            2 if is_alive => true,
+                            3 => true,
+                            _ => false,
+                        }
+                    }).collect()
             }).collect();
     }
-    fn generate_random(size: u32) -> Vec<bool> {
-        (0..size)
-            .map(|_| rand::random::<bool>())
-            .collect::<Vec<bool>>()
+    fn generate_random(width: u32, height: u32) -> CellMap {
+        (0..width)
+            .map(|_| (0..height).map(|_| rand::random::<bool>()).collect())
+            .collect()
     }
 }
 
@@ -122,20 +117,19 @@ fn start_gol<'a>(application: &'a gtk::Application, cells: Rc<RefCell<Cells>>) {
     window.show_all();
 }
 
-fn draw_gol(ctx: &Context, cells: &[bool]) -> gtk::Inhibit {
-    for (i, item) in cells.iter().enumerate() {
-        if *item {
-            draw_cell(ctx, i as i32 % 128, i as i32 / 128, 5);
+fn draw_gol(ctx: &Context, cells: &CellMap) -> gtk::Inhibit {
+    for (x, col) in cells.iter().enumerate() {
+        for (y, cell) in col.iter().enumerate() {
+            if *cell {
+                draw_cell(ctx, x as f64, y as f64, 5_f64);
+            }
         }
     }
     Inhibit(false)
 }
 
-fn draw_cell(ctx: &Context, x: i32, y: i32, size: i32) {
-    let f_size: f64 = size.into();
-    let f_x: f64 = x.into();
-    let f_y: f64 = y.into();
+fn draw_cell(ctx: &Context, x: f64, y: f64, size: f64) {
     ctx.set_source_rgb(0.0, 0.0, 0.0);
-    ctx.rectangle(f_x * f_size, f_y * f_size, f_size, f_size);
+    ctx.rectangle(x * size, y * size, size, size);
     ctx.fill();
 }
